f744397e1532e7eab00e327164db6768ac0161b5
FaustinoEdu <kingdosemdermam@gmail.com>
2023-09-12 19:37:57 -0400

    Estruturando a atividade

--- web2_aula_28_08_2023/js.js
+++ web2_aula_28_08_2023/js.js
@@ -1,61 +1,14 @@
-var opPeso = 1;
-var opAltura = 2;
-var contadoraElements = document.getElementsByTagName("td").length;
-var opImc = 4;
-var opCla = 5;
+//document.getElementById("demo").style.color = "red";
 
+var criandoColuna = document.createElement("th");
+var criandoLinha= document.createElement("td");
 
-var colunaClassifi = document.createElement("th");
-var descClassifi = document.createTextNode("Classificação");
+const collection = document.getElementsByClassName("");// Buscando por classe
 
+//var content = element.innerHTML; permite modificar elementos de um html
 
+//const buscandoImclinha= document.getElementsByTagName("td").getElementById("imc");
+//const box = document.querySelector("#imc"); busca pelo id, pesquisar sobre o query
 
-
-for(let i=0; i < contadoraElements; i++){
-
-  if(i==4){
-    document.getElementById("tabelas").appendChild(x); ;
-  }
-
-
-  document.createElement("td");
-  
-}
-
-
-for (let i = 0; i < contadoraElements; i++) {
-  var peso = document.getElementsByTagName("td")[opPeso].innerHTML;
-  var altura = document.getElementsByTagName("td")[opAltura].innerHTML;
-
-  var imc = peso / (altura ^ 2)
-
-  document.getElementsByTagName("td")[opImc].innerHTML = imc;
-
-
-  if (imc > 25) {
-
-
-
-  }
-  else {
-    if (imc >= 18, 5) {
-
-
-    }
-    else {
-      if (imc > 0) {
-
-
-      }
-
-
-    }
-  }
-
-
-  opPeso = opPeso + 6;
-  opAltura = opAltura + 6;
-  opImc = opImc + 6;
-  opCla = opCla + 6;
-}
-
+/*const box = document.getElementById("box");
+box.innerHTML = "<p>Box</p>";*/
