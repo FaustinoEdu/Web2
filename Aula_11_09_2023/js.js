a504a4c6672f5a90bc4108302cf97d9956a88a7c
FaustinoEdu <kingdosemdermam@gmail.com>
2023-09-18 19:43:44 -0400

    Adicionando o arquivo tyopscript e criando o metodo get

--- Aula_11_09_2023/js.js
+++ Aula_11_09_2023/js.js
@@ -1,10 +1,20 @@
 async function postagem(){
  let postagem= document.querySelector('#postagem');
- let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+ let response = await fetch('https://jsonplaceholder.typicode.com/posts');//Fetch ->Cria uma promessa para carregar alguns registros . Await espera a promessa
  let json= await response.json();
  console.log(json[1])
 
 
 }
-postagem()
+if (json.length){
+postagem.innerHTML= '';
+for(let i in json){
+ let postsHTML ='<div><h1>'+json[i].title+'</h1>'+json[i].body+'</div>';
+ postagem.innerHTML=postagem.innerHTML+postsHTML;
 
+}
+
+
+
+}
+postagem()
