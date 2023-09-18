9f615395b0e48261d37a450571875cefc1c68d5d
FaustinoEdu <kingdosemdermam@gmail.com>
2023-09-18 12:27:38 -0400

    Verção teste

--- web2_aula_28_08_2023/js.js
+++ web2_aula_28_08_2023/js.js
@@ -1,7 +1,17 @@
-var novosTds = 4;
-var classificacao;
+var posicaoImc = 4;
+var posicaoClassificacao=5;
+var posicaoAltura=2;
+var posicaoPeso=1;
+
+var classificacao= getElementsByTagName[posicaoClassificacao];
+var imc= percorrendoTabela[posicaoImc];
+
+
 
 var minhaTabela = document.querySelector("table");//Selecioana elementos 
+//Percorrendo as tabelas
+var percorrendoTabela= document.getElementsByTagName("td"); 
+
 
 // Adicione a nova coluna de cabeçalho
 var novaColunaCabecalho = document.createElement("th"); // Criando novo cabeçalho da coluna
@@ -17,3 +27,61 @@ linhas.forEach(function (linha) {//forEach passa uma função de callback
   linha.appendChild(novaColunaDados);
 
 });
+
+
+for(var i; i<42; i++){
+
+var peso= percorrendoTabela[posicaoPeso]
+var altura= percorrendoTabela[posicaoAltura]
+
+
+imc.textContent= calculoIMC(peso, altura);
+
+
+ classificacao.textContent= conteudoClassificacao(imc, posicaoClassificacao);
+
+
+posicaoImc += 6;
+posicaoClassificacao+=6;
+posicaoAltura+=6;
+posicaoPeso+=6;
+} 
+
+function calculoIMC(altura, peso){
+var imc= peso/(altura^2)
+return imc;
+}
+
+
+function conteudoClassificacao(imc, posicaoClassificacao){
+classificacao= getElementsByTagName[posicaoClassificacao];
+
+  if(imc<24.99){
+if(imc<18,5){//Baixo Peso
+
+classificacao.textContent= "Baixo Peso";
+classificacao.style.backgroundColor = "green";
+
+}else
+{ // normal
+
+  classificacao.textContent= "Normal";
+  classificacao.style.backgroundColor = "Yellow";
+}
+}
+else{
+if(imc>25){
+
+  if(imc<29.99){//Sobrepeso
+    classificacao.textContent=  "Sobrepeso";
+    classificacao.style.backgroundColor = "Orange";
+  }else{//Obesidade
+    classificacao.textContent= "Obesidade";
+    classificacao.style.backgroundColor = "Red";
+  }
+
+}
+}
+
+}
+
